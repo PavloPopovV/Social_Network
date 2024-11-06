1f3c30bd381d8e1ffe26118e2a1ebd7fed6f3377
PavloPopovV <pavlopopov570@gmail.com>
2024-11-06 12:42:41 +0200

    changes

--- backend/server.js
+++ backend/server.js
@@ -11,20 +11,20 @@ const __dirname = path.resolve();
 dotenv.config();
 
 const corsOptions = {
-  origin: ['http://localhost:5173'], // Ваш клієнт
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Дозволені методи
-  allowedHeaders: ['Content-Type', 'Authorization'], // Дозволені заголовки
+  origin: ["http://localhost:5173", "https://social-network-qptv.onrender.com"], // Ваш клієнт
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Дозволені методи
+  allowedHeaders: ["Content-Type", "Authorization"], // Дозволені заголовки
   credentials: true, // Якщо потрібно передавати куки
 };
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json()); 
+app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
 
-app.use("/api", router); 
+app.use("/api", router);
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 app.get("*", (req, res) => {
